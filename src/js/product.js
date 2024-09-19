9e9d0142e6f9c337a71c80486a15e5a6dd9ece7a
Daniel Concha <danielconchaparra@gmail.com>
2024-09-19 13:15:18 -0300

    Fix: Individual Activity W01: Add item to cart broken

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,18 @@
-import { setLocalStorage } from './utils.mjs';
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  const storedCartState = getLocalStorage('so-cart');
+
+  // If there is nothing on local storage with this key, it will return null,
+  // on which case we will assign it an empty array,
+  // otherwise, we return its contents
+  const newCartState = storedCartState === null ? [] : storedCartState;
+
+  // We set to local storage the previous contents plus the new product
+  setLocalStorage('so-cart', [...newCartState, product]);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
