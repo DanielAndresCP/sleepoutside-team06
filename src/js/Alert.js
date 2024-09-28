a590dd77ee3fe44981ea72d8725322b7007865f4
Daniel Concha <danielconchaparra@gmail.com>
2024-09-27 23:36:30 -0300

    Finalize Alert class functionality

--- src/js/Alert.js
+++ src/js/Alert.js
@@ -1,22 +1,34 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+function renderSingleAlert(alertObj) {
+  return `<p style="background-color:${alertObj.background}; color:${alertObj.color};">${alertObj.message}</p>`;
+}
+
 export default class Alert {
-    constructor() {
-        this.alertsFilepath = "../json/alerts.json";
+  constructor() {
+    this.alertsFilepath = "../json/alerts.json";
+  }
+
+  async init() {
+    const alerts = await this.fetchAlerts();
+
+    if (alerts.lenght === 0) {
+      return;
     }
 
-    
+    // Since renderListWithTemplate needs a real HTML element,
+    // we must use these lines to create one instead of using template strings
+    const alertsContainer = document.createElement("section");
+    alertsContainer.classList.add("alert-list");
 
-    async init() {
-        const alerts = await this.fetchAlerts()
+    renderListWithTemplate(renderSingleAlert, alertsContainer, alerts);
 
-        if (alerts.lenght === 0) {
-            return
-        }
+    // We add the created element to the main element on the document
+    document.querySelector("main").prepend(alertsContainer);
+  }
 
-
-    }
-
-    async fetchAlerts() {
-        const response = await fetch(this.alertsFilepath);
-        return await response.json();
-    }
+  async fetchAlerts() {
+    const response = await fetch(this.alertsFilepath);
+    return await response.json();
+  }
 }
