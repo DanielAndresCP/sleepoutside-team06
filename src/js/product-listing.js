eb5ec58d642a3c6ae2fdc4cef1312c64d278bc64
Daniel Concha <danielconchaparra@gmail.com>
2024-09-30 22:56:32 -0300

    Make product listing title dynamic based on category

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -2,11 +2,17 @@ import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 import { getParams } from "./utils.mjs";
 
+const category = getParams("category")
+
 const dataSource = new ProductData();
 const productList = new ProductListing(
-  getParams("category"),
+  category,
   dataSource,
   document.querySelector(".product-list"),
 );
 
 productList.init();
+
+// This line takes the category that comes on the parameter, replaces hyphens with spaces, and makes the first letter uppercase
+const prettyCategoryName = category.replace("-"," ").slice(0,1).toLocaleUpperCase() + category.replace("-"," ").slice(1)
+document.querySelector(".products > h2").textContent = `Top Products: ${prettyCategoryName}` 
