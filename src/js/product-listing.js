81150970da3a8cad498f84fe5abd666f03a08d20
Daniel Concha <danielconchaparra@gmail.com>
2024-09-30 22:57:22 -0300

    Format changes

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -2,7 +2,7 @@ import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 import { getParams } from "./utils.mjs";
 
-const category = getParams("category")
+const category = getParams("category");
 
 const dataSource = new ProductData();
 const productList = new ProductListing(
@@ -14,5 +14,8 @@ const productList = new ProductListing(
 productList.init();
 
 // This line takes the category that comes on the parameter, replaces hyphens with spaces, and makes the first letter uppercase
-const prettyCategoryName = category.replace("-"," ").slice(0,1).toLocaleUpperCase() + category.replace("-"," ").slice(1)
-document.querySelector(".products > h2").textContent = `Top Products: ${prettyCategoryName}` 
+const prettyCategoryName =
+  category.replace("-", " ").slice(0, 1).toLocaleUpperCase() +
+  category.replace("-", " ").slice(1);
+document.querySelector(".products > h2").textContent =
+  `Top Products: ${prettyCategoryName}`;
