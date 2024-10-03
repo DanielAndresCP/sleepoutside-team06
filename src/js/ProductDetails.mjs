63e95b3083b1e58210bc7fcdf261313615266cda
Daniel Concha <danielconchaparra@gmail.com>
2024-10-03 19:46:35 -0300

    Add discount badge to single product page

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,21 +1,33 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function productDetailsTemplate(product) {
-    return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
-      <h2 class="divider">${product.NameWithoutBrand}</h2>
-      <img
-        class="divider"
-        src="${product.Image}"
-        alt="${product.NameWithoutBrand}"
-      />
-      <p class="product-card__price">$${product.FinalPrice}</p>
-      <p class="product__color">${product.Colors[0].ColorName}</p>
-      <p class="product__description">
-      ${product.DescriptionHtmlSimple}
-      </p>
-      <div class="product-detail__add">
-        <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-      </div></section>`;
+    const isDiscounted = product.FinalPrice < product.SuggestedRetailPrice
+
+    if (isDiscounted) {
+        const priceDiff = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(product.SuggestedRetailPrice - product.FinalPrice)
+        var discountBadgeHTML = `<span class="product-discount-badge">${priceDiff} OFF</span>`
+    }
+
+
+    return `
+    <section class="product-detail">
+        <h3>${product.Brand.Name}</h3>
+        <h2 class="divider">${product.NameWithoutBrand} ${isDiscounted ? discountBadgeHTML : ""}</h2>
+        <img
+            class="divider"
+            src="${product.Image}"
+            alt="${product.NameWithoutBrand}"
+        />
+        <p class="product-card__price">$${product.FinalPrice}</p>
+        <p class="product__color">${product.Colors[0].ColorName}</p>
+        <p class="product__description">${product.DescriptionHtmlSimple}</p>
+        <div class="product-detail__add">
+            <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+        </div>
+    </section>`;
 }
 
 export default class ProductDetails {
