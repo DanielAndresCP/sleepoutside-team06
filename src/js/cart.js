60ca31777a3cd112d2bb4d3d305317b613104c04
Daniel Concha <danielconchaparra@gmail.com>
2024-10-04 21:17:48 -0300

    Formatting changes

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,5 +1,5 @@
 import ShoppingCart from "./ShoppingCart.mjs";
-import {  loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const shoopingCart = new ShoppingCart("so-cart", ".product-list");
 
