383e9821ac7a7beb4040bc2b72e88c6dd68c9c9b
Daniel Concha <danielconchaparra@gmail.com>
2024-10-10 21:46:52 -0300

    Create and initialize CheckoutProcess class

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,9 @@
 import { loadHeaderFooter } from "./utils.mjs";
+import CheckoutProcess from "./CheckoutProcess.mjs"
+
 
 loadHeaderFooter();
+
+const checkoutProcess = new CheckoutProcess("so-cart", ".order-result")
+
+checkoutProcess.init()
