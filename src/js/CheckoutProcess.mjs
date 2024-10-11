05402771f5522b1b447e7570ab30b1107cda87c9
Daniel Concha <danielconchaparra@gmail.com>
2024-10-10 22:47:36 -0300

    Finish checkout result showing

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,4 +1,5 @@
 import { getLocalStorage } from "./utils.mjs"
+import { getMoneyString } from "./utils.mjs";
 
 export default class CheckoutProcess {
     constructor(key, outputSelector) {
@@ -6,6 +7,7 @@ export default class CheckoutProcess {
         this.outputSelector = outputSelector;
         this.list = [];
         this.itemTotal = 0;
+        this.itemsQuantity = 0;
         this.shipping = 0;
         this.tax = 0;
         this.orderTotal = 0;
@@ -14,15 +16,39 @@ export default class CheckoutProcess {
     init() {
         this.list = getLocalStorage(this.key);
         this.calculateItemSummary();
+
+        document.querySelector("#zip-code").addEventListener("blur", () => { this.calculateOrdertotal() })
     }
 
     calculateItemSummary() {
         // calculate and display the total amount of the items in the cart, and the number of items.
+        const itemAmountElement = document.querySelector(`${this.outputSelector} #result-item-amount`)
+        this.itemsQuantity = this.list.map((x) => x.quantity).reduce((acc, x) => acc + x)
+        itemAmountElement.textContent = this.itemsQuantity
 
+        const orderSubtotalElement = document.querySelector(`${this.outputSelector} #result-subtotal`)
+        this.itemTotal = this.list.map((x) => x.FinalPrice * x.quantity).reduce((acc, x) => acc + x)
+        orderSubtotalElement.textContent = getMoneyString(this.itemTotal)
+
+        // const amounts = this.list.map((item) => item.FinalPrice);
+        // this.itemTotal = amounts.reduce((sum, item) => sum + item);
+        // summaryElement.innerText = "$" + this.itemTotal;
     }
+
 
     calculateOrdertotal() {
         // calculate the shipping and tax amounts. Then use them to along with the cart total to figure out the order total
+        // Tax: Use 6% sales tax.
+        // Shipping: Use $10 for the first item plus $2 for each additional item after that.
+
+        const TAX_PERCENT = 0.06
+        this.tax = this.itemTotal * TAX_PERCENT
+
+        const SHIPPING_BASE = 10
+        const SHIPPING_EXTRA = 2
+        this.shipping = SHIPPING_BASE + ((this.itemsQuantity - 1) * SHIPPING_EXTRA)
+
+        this.orderTotal = this.tax + this.shipping + this.itemTotal
 
         // display the totals.
         this.displayOrderTotals();
@@ -30,6 +56,13 @@ export default class CheckoutProcess {
 
     displayOrderTotals() {
         // once the totals are all calculated display them in the order summary page
+        const taxAmountElement = document.querySelector(`${this.outputSelector} #result-tax`)
+        taxAmountElement.textContent = getMoneyString(this.tax)
 
+        const shippingAmountElement = document.querySelector(`${this.outputSelector} #result-shipping-estimate`)
+        shippingAmountElement.textContent = getMoneyString(this.shipping)
+        
+        const orderTotalElement = document.querySelector(`${this.outputSelector} #result-order-total`)
+        orderTotalElement.textContent = getMoneyString(this.orderTotal)
     }
 }
